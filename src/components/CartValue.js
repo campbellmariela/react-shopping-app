13ed1c283e90aaeea67df71b148cdbc5c39503aa
campbellmariela <marielakmpbell@gmail.com>
2024-04-04 15:54:48 -0300

    Functional shopping cart

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,7 +1,18 @@
+// Shows the total price of all the items added in the cart
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const { expenses, Location } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Cart Value: {Location}{totalExpenses}</span>
+        </div>
+    );
 };
 
 export default CartValue;
